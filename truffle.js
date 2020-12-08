d3fc593c5cd673840797508807b069c26fd74940
Heinrich Malan <heinrich@heinbio.me>
2020-12-08 16:49:35 +0100

    fix development truffle settings

--- truffle.js
+++ truffle.js
@@ -1,20 +1,13 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic =
     "involve hero zero valid donor august cycle layer song liquid year network";
-
+var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
 module.exports = {
     networks: {
         development: {
-            provider: function () {
-                return new HDWalletProvider(
-                    mnemonic,
-                    "http://127.0.0.1:8545/",
-                    0,
-                    50
-                );
-            },
-            network_id: "*",
-            gas: 6721975,
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*", // Match any network id
         },
     },
     compilers: {
